6bd666e75b6fb8240c9bdf9ef02cfd7088e14874
CamiAilenn <camilafrosio@gmail.com>
2024-11-30 14:43:39 -0300

    Candy Crush

---
+++ Candy_Crush.py
@@ -0,0 +1,139 @@
+# #Recientemente Candy Crush Saga le pide una variación del juego a U.T.N para 
+# su juego de consola con los siguientes requerimientos:
+#Dada la siguiente lista de diccionarios
+#● lista = [
+#● {"piezas":[]},
+#● {"piezas":[]},
+#● {"piezas":[]},
+#● {"piezas":[]}
+#● ]
+#
+#Desarrollar una función que genere números al azar entre 1 y 3, y completar las 
+# listas para que queden de la siguiente forma:
+# Ej:
+# ● [2, 3, 3, 3, 3, 3, 1]
+# ● [1, 1, 1, 1, 1, 1, 3]
+# ● [3, 1, 2, 3, 1, 1, 1]
+# ● [1, 1, 3, 2, 3, 2, 2]
+# (los número son generados al azar)
+#
+#
+#Solicitar al usuario una posición (fila y una columna)
+#Verificar si, de forma vertical, existen desde esa posición elegida, 
+#hacia arriba o hacia abajo 3 números iguales, es decir 3 unos, 3 dos o 3 tres.
+#Si existen mostra el mensaje "HA GANADO 10 PUNTOS", sino "SEGUI PARTICIPANDO"
+# Utilizar funciones, validar el ingreso de datos
+
+from random import *
+
+def generar_tablero(filas, columnas, clave, inicio_random:int, fin_random:int)->list:
+    
+    matriz = []
+    for i in range(filas):
+        diccionario_filas = {clave:[]}
+        matriz.append(diccionario_filas) 
+        for j in range(columnas):
+            numero = randint(inicio_random,fin_random)
+            matriz[i][clave].append(numero)
+    return matriz
+
+def validar_rango(numero, desde, hasta)->float | int:
+    """Valida el numero en un rango, y lo pide indefinidamente hasta que este validado
+    Una vez validado, lo retorna"""
+    while numero< desde or numero > hasta:
+        numero = int(input(f"Ingreso, no valido. Ingrese un numero ({desde+1}-{hasta+1}): "))
+    return numero
+
+
+def ingresar_validar_fila(desde=0, hasta=9)->int:
+    """Solicita y valida una posicion(fila) al usuario"""
+    
+    fila = int(input(f"¿En que fila se encuentra? ({desde+1}/{hasta+1})"))
+    fila = validar_rango(fila-1, desde, hasta)   
+    return fila 
+
+def ingresar_validar_columna(desde=0, hasta=9)->int:
+    """Solicita y valida una posicion(columna) al usuario"""
+    columna = int(input(f"¿En que columna se encuentra? ({desde+1}/{hasta+1})"))
+    columna = validar_rango(columna-1, desde, hasta)
+    return columna
+
+def mostrar_lista(lista:list):
+    """Ingresa una lista por parametro y la muestra con un for"""
+    for e_lista in lista:
+        print(e_lista)
+
+
+def numero_en_posicion (lista, fila, clave, columna)->int:
+    """Ingresa por parametro una lista, una fila, una clave y una columna.
+    y retorna el numero que se encuentra en esa posicion"""
+    numero = lista[fila][clave][columna]
+    return numero
+
+
+def revisar_fila_anterior (tablero, fila, clave, columna, numero)->bool:
+    """Ingresa el tablero, la posicion (fila y columna)
+    y el numero con el que compara
+    Devuelve True si lo encuentra en la misma columna pero la fila anterior"""
+    if tablero[fila + 1][clave][columna] == numero:
+        return True
+    
+def revisar_fila_siguiente (tablero, fila, clave, columna, numero)->bool:
+    """Ingresa el tablero, la posicion (fila y columna)
+    y el numero con el que compara
+    Devuelve True si lo encuentra en la misma columna pero la fila siguiente"""
+    if tablero[fila - 1][clave][columna] == numero:
+        return True
+    
+filas = 4
+columnas = 7
+clave = "Piezas"
+inicio_piezas = 1
+fin_piezas = 3
+bandera_ganador = False
+
+'''
+tablero = generar_tablero(filas, columnas, clave, inicio_piezas, fin_piezas)
+mostrar_lista(tablero)
+print("Usted ingresara una posicion")
+fila = ingresar_validar_fila(0,3)
+columna = ingresar_validar_columna(0,6)
+numero =  numero_en_posicion(tablero, fila, clave, columna)  #Guarda el numero en la posicion ingresada por el usuario
+print(f"La fila elegida es {fila}")         #Chequeo
+print(f"La columna elegida es {columna}")   #Chequeo
+print(numero)                               #Chequeo
+
+#------------------------------------------------------------
+if fila == 0:
+    if revisar_fila_siguiente(tablero, fila, clave, columna, numero):
+        fila += 1  #Para usar la funcion y que revise la fila siguiente de la siguiente
+        if revisar_fila_siguiente(tablero, fila, clave, columna, numero):
+            bandera_ganador = True
+            
+elif fila == 1: 
+    if revisar_fila_anterior(tablero, fila, clave, columna, numero):
+        if revisar_fila_siguiente(tablero, fila, clave, columna, numero):
+            bandera_ganador = True
+    elif revisar_fila_siguiente(tablero, fila, clave, columna, numero):
+        fila += 1  #Para usar la funcion y que revise la fila siguiente de la siguiente
+        if revisar_fila_siguiente(tablero, fila, clave, columna, numero):
+            bandera_ganador = True
+elif fila == 2:
+    if revisar_fila_siguiente(tablero, fila, clave, columna, numero):
+        if revisar_fila_anterior(tablero, fila, clave, columna, numero):
+            bandera_ganador = True
+    elif revisar_fila_anterior(tablero, fila, clave, columna, numero):
+        fila -=1
+        if revisar_fila_anterior(tablero, fila, clave, columna, numero):
+            bandera_ganador = True
+else:
+    if revisar_fila_anterior(tablero, fila, clave, columna, numero):
+        fila -= 1   #Para usar la funcion y que revise la fila ANTERIOR de la anterior
+        if revisar_fila_anterior(tablero, fila, clave, columna, numero):
+            bandera_ganador = True
+
+
+if bandera_ganador:
+    print("HA GANADO 10 PUNTOS")
+else:
+    print("SEGUI PARTICIPANDO")'''
---
+++ Pygame.py
@@ -0,0 +1,104 @@
+import pygame
+from colores import *
+from Candy_Crush import *
+print(BLUE2)
+pygame.init()       #iniciar la pantalla
+#declarar constantes
+ANCHO_VENTANA = 1200
+ALTO_VENTANA = 700
+
+filas = 4
+columnas = 7
+clave = "Piezas"
+inicio_piezas = 1
+fin_piezas = 3
+bandera_ganador = False
+lista_caramelos_uno = []
+lista_caramelos_dos = []
+lista_caramelos_tres = []
+
+pantalla = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))   # Creo la pantalla
+pygame.display.set_caption("Candy Dulce Crush")     # Pongo un titulo en la ventana
+
+#IMAGENES
+imagen_caramelo_uno = pygame.image.load("caramelo_uno.png")
+imagen_caramelo_dos = pygame.image.load("caramelo_dos.jpg")
+imagen_caramelo_tres = pygame.image.load("caramelo_tres.png")
+
+imagen_caramelo_uno = pygame.transform.scale(imagen_caramelo_uno,(100,100))
+imagen_caramelo_dos = pygame.transform.scale(imagen_caramelo_dos,(100,100))
+imagen_caramelo_tres =pygame.transform.scale(imagen_caramelo_tres,(100,100))
+
+tablero = generar_tablero(filas, columnas, clave, inicio_piezas, fin_piezas)
+#mostrar_lista(tablero)
+
+
+posicion_mouse = pygame.Rect(0,0,6,6)
+
+
+
+
+
+flag_correr = True          # El juego corre mientras la bandera esta True
+while flag_correr:
+
+    lista_eventos = pygame.event.get()      # Guardo los eventos de la ventada en una lista
+    for evento in lista_eventos:
+        if evento.type == pygame.QUIT:      # Si el tipo de evento es SALIR (detecta si el usuario cierra la ventana)
+            flag_correr = False
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            posicion_click = list(evento.pos)     #print(evento.pos)  me imprime la posicion donde se hizo click
+            posicion_mouse[0] = posicion_click[0] #modifico el left del rect
+            posicion_mouse[1] = posicion_click[1]
+      
+    
+
+    pantalla.fill(MAGENTA3)    # Pongo un fondo de color en la pantalla principal  
+
+    for i in range(4):
+        for j in range(7): 
+            posicion_caramelo = [(j+1) *125, (i+1) *125]
+            posicion_rect_caramelo = pygame.Rect(posicion_caramelo[0],posicion_caramelo[1],6,6)
+            if numero_en_posicion(tablero, i, clave, j) == 1:
+                pantalla.blit(imagen_caramelo_uno,posicion_caramelo)
+                lista_caramelos_uno.append(posicion_caramelo)
+
+            elif numero_en_posicion(tablero, i, clave, j) == 2: 
+                pantalla.blit(imagen_caramelo_dos,(posicion_caramelo))
+                lista_caramelos_dos.append(posicion_caramelo)
+                
+            else:
+                pantalla.blit(imagen_caramelo_tres,(posicion_caramelo))
+                lista_caramelos_tres.append(posicion_caramelo)
+   
+    for i in range(len(lista_caramelos_uno)):
+        posicion_caramelo_uno = pygame.Rect(lista_caramelos_uno[i][0],lista_caramelos_uno[i][1], 10, 10)
+        rect_caramelo_uno = imagen_caramelo_uno.get_rect()
+        rect_caramelo_uno.x = lista_caramelos_uno[i][0] 
+        rect_caramelo_uno.y = lista_caramelos_uno[i][1] 
+        if posicion_mouse.colliderect(rect_caramelo_uno):
+            posicion_circulo = [1100, 200]
+            pygame.draw.circle(pantalla, GREEN2, posicion_circulo,80)
+
+    for i in range(len(lista_caramelos_dos)):
+        posicion_caramelo_dos = pygame.Rect(lista_caramelos_dos[i][0],lista_caramelos_dos[i][1], 10, 10)
+        rect_caramelo_dos = imagen_caramelo_dos.get_rect()
+        rect_caramelo_dos.x = lista_caramelos_dos[i][0] 
+        rect_caramelo_dos.y = lista_caramelos_dos[i][1] 
+        if posicion_mouse.colliderect(rect_caramelo_dos):
+            posicion_circulo = [1100, 400]
+            pygame.draw.circle(pantalla, RED1 ,posicion_circulo,80)
+    
+    for i in range(len(lista_caramelos_tres)):
+        posicion_caramelo_tres = pygame.Rect(lista_caramelos_tres[i][0],lista_caramelos_tres[i][1], 10, 10)
+        rect_caramelo_tres = imagen_caramelo_tres.get_rect()
+        rect_caramelo_tres.x = lista_caramelos_tres[i][0] 
+        rect_caramelo_tres.y = lista_caramelos_tres[i][1] 
+        if posicion_mouse.colliderect(rect_caramelo_tres):
+            posicion_circulo = [1100, 600]
+            pygame.draw.circle(pantalla, COLOR_AMARILLO,posicion_circulo,80)
+
+
+    pygame.display.flip()         # Muestro los cambios en la pantalla
+
+pygame.quit()       # Termina el programa
